69aed0a50a6a065be1c0e0e037fd97874f3d5f07
App Academy Student <email@email.com>
2022-04-08 14:46:12 -0400

    started phase1

---
+++ widgets/widgets.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("main");
+  const hello = <h1>Hello</h1>;
+
+  ReactDOM.render(hello, root)
+});
